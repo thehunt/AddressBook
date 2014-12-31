19ca2394c285e499f6690ac9ae58c3d5f1c89d06
LiPu <116678732@qq.com>
2014-12-31 17:46:28 +0800

    add first

--- cloud/main.js
+++ cloud/main.js
@@ -2,4 +2,6 @@
 // For example:
 AV.Cloud.define("hello", function(request, response) {
   response.success("Hello world!");
+  AV._initialize("tekdkklqguiw626ai8d7xu5j7fu6fl2q628x7wlucys1rklm", "ro9uqow59iojuamtmsaooq9tvw4q9hc18ylwwrfwhwy7sk2f", "g1npeuzzre4eosychmkoq97o8sl0hcwevx1ofyhk9113fqal");
+  AV.Cloud.useMasterKey();
 });
